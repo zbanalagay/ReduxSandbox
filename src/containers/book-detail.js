cab49b4274174607769b4478afa2dedd480b4070
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-03-31 16:52:29 -0700

    added a if clause for when the app is first booted up in book-detail.js

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -3,8 +3,14 @@ import {connect} from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+    if(!this.props.book){
+      return <div>Select a book to get started </div>
+    }
     return(
-      <div>Book Detail! </div>
+      <div>
+        <h3> Details for: </h3>
+        <div>{this.props.book.title}</div>
+      </div>
     )
   }
 }
