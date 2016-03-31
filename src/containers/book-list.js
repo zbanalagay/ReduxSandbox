9d739389ee8c05b5da81eaa5ef27d7ec8a8b806c
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-03-31 16:21:58 -0700

    added onClick event in book-list.js

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -7,7 +7,12 @@ class BookList extends Component {
   renderList(){
       return this.props.books.map((book)=> {
         return (
-          <li key={book.title} className='list-group-item'>{book.title}</li>
+          <li
+            key={book.title}
+            onClick={()=> this.props.selectBook(book)}
+            className='list-group-item'>
+            {book.title}
+          </li>
         )
       });
   }
@@ -35,4 +40,6 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators({selectBook : selectBook}, dispatch)
 }
 
+//Promote BookList from a component to a container - it needs to know about this new dispatch
+//method, selectBook. Make it available as a prop.
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);
